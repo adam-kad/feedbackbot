f66cda09af9f7544684d5aef11ff79640a95f072
Adam <mgm.kadiev@gmail.com>
2024-07-31 23:03:27 +0300

    check_admin_status

--- bot/logic/callbacks.py
+++ bot/logic/callbacks.py
@@ -1,6 +1,15 @@
 from bot.settings import bot, TELEGRAM_GROUP_ID
 
 
+def check_admin_status(func):
+    def wrapper(call):
+        chat_member = bot.get_chat_member(TELEGRAM_GROUP_ID, call.from_user.id)
+        if chat_member.status in ['administrator', 'creator']:
+            return func(call)
+
+    return wrapper
+
+
 def remove_feedback_callback(call):
     username = call.message.text.split("\n")[0][4:]
 
@@ -11,6 +20,7 @@ def remove_feedback_callback(call):
     )
 
 
+@check_admin_status
 def answer_feedback_callback(call):
     data = call.data.split("_")
 
